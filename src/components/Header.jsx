745956ddeefde11e0c31e173b11ff8b05dd7101e
karthikeyagrad29-arch <karthikeyagrad29@gmail.com>
2026-09-03 12:10:04 +0000

    Had to restart from scratch, and inserted images.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
+import robotsImage from "../assets/robots.png"
+
 function Header() {
   return (
     <section className="header">
-      <h1>The Impact of Artificial Intelligence on Society</h1>
+      <h1>Deepfleet: AI that powers the world</h1>
 
       <p>
-        Exploring how AI affects education, healthcare,
-        business, and everyday life.
+          AI can help the world...
       </p>
+      <img className="header-image" src={robotsImage} alt="Robots working in an Amazon warehouse" />
     </section>
-  );
+  )
 }
 
 export default Header;
